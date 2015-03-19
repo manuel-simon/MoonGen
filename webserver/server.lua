44cf79eaa3efe1b6281c332f2383488ca637bbfd
gallenmu <gallenmu@net.in.tum.de>
2015-03-19 13:51:58 +0000

    add server & right fixes

--- webserver/server.lua
+++ webserver/server.lua
@@ -1,5 +1,17 @@
-local Pegasus = require 'pegasus'
+local turbo = require "turbo"
 
-local server = Pegasus:new('80')
+local port = 80
 
-server:start()
+local app = turbo.web.Application:new({
+	-- Serve single index.html file on root requests.
+	{"^/$", turbo.web.StaticFileHandler, "./index.html"},
+	-- Serve contents of directory.
+	{"^/(.*)$", turbo.web.StaticFileHandler, "./"}
+})
+
+local srv = turbo.httpserver.HTTPServer(app)
+srv:bind(port)
+srv:start(1) -- Adjust amount of processes to fork.
+print('server listening on port: ' .. port)
+
+turbo.ioloop.instance():start()
