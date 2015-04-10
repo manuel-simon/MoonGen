8a0f70a9dccc2a2f9763f4943ab2a29985c226b6
Sebastian Gallenmüller <gallenmu@net.in.tum.de>
2015-04-10 15:14:23 +0200

    fix display flaw

--- examples/webserver/app.js
+++ examples/webserver/app.js
@@ -19,6 +19,9 @@ $scope.addData = function() {
 				console.log(data);
 				data.x=$scope.data.length;
 				$scope.data.push(data);
+                if ($scope.data.length > 60) {
+                    $scope.data.splice(0, 1);
+                }
 				$scope.options.axes.x.min = data.x - 60;
 				$scope.options.axes.x.max = data.x;
 				$scope.latestLatency = data.y;
